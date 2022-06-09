8293d071bd1ce93d97b68033c02faaf89963aa0f
LeoDog896 <LeoDog896@hotmail.com>
2022-06-09 16:42:56 +0000

    Begin blob

--- src/lib/Blob.svelte
+++ src/lib/Blob.svelte
@@ -1,1 +1,44 @@
-<h1></h1>
+<script lang="ts">
+  interface Position { x: number, y: number };
+
+  const random = (min: number, max: number) => Math.random() * (max - min) + min;
+  const transformedPoint = ({ x, y }: Position): Position => ({ x: (size / 3) * x + size / 2, y: (size / 3) * y + size / 2}) 
+
+  export let pointAmount = 6;
+  export let size = 100;
+  export let debugCircles = false;
+  export let debugCircleFill = "pink";
+  export let debugCircleRadius = 3;
+  export let deformation = 0;
+  export let randomProfile: Position[] = []
+  export let path = ""
+
+  $: randomProfile = Array(pointAmount).fill(0).map(() => ({ x: random(-deformation, deformation), y: random(-deformation, deformation) }))
+
+  $: points = Array(pointAmount)
+    .fill(0).map((_, i) => i * (Math.PI * 2) / pointAmount)
+    .map(radius => ({ x: Math.cos(radius), y: Math.sin(radius) }))
+  
+  $: deformedPoints = points.map(({ x, y }, i) => ({ x: x + randomProfile[i].x, y: y + randomProfile[i].y }))
+
+  $: transformedDeformedPoints = deformedPoints.map(transformedPoint)
+
+  $: path = `M ${transformedDeformedPoints[0].x} ${transformedDeformedPoints[0].y} Q ` + 
+    transformedDeformedPoints.slice(1).map(({ x, y }) => `${x} ${y}`).join(", ") +
+    ` T ${transformedDeformedPoints[0].x} ${transformedDeformedPoints[0].y}`
+</script>
+<svg on:click on:mousemove on:mouseover on:mouseleave on:focus on:blur width={size} height={size} >
+  {#if debugCircles}
+    {#each points as point, i}
+      {@const { x, y } = transformedPoint(point)}
+      {@const { x: ax, y: ay } = transformedPoint(deformedPoints[i])}
+      <circle cx={x} cy={y} r={debugCircleRadius} fill="gray" opacity="0.3"></circle>
+      <line x1={x} y1={y} x2={ax} y2={ay} stroke="black"></line>
+    {/each}
+    {#each deformedPoints as point}
+      {@const { x, y } = transformedPoint(point)}
+      <circle cx={x} cy={y} r={debugCircleRadius} fill={debugCircleFill}></circle>
+    {/each}
+  {/if}
+  <path fill="transparent" stroke="black" d={path}></path>
+</svg>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,42 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import Blob from "$lib/Blob.svelte"
+
+  let pointAmount = 6
+  let debugCircles = false
+  let debugCircleRadius = 3
+  let deformation = 0.1
+  let key = {}
+  let path = ""
+</script>
+
+<div class="m-8">
+  <div class="p-8 rounded-lg border border-gray-300 inline-block">
+    {#key key}
+      <Blob {pointAmount} {debugCircles} bind:path={path} {debugCircleRadius} {deformation}></Blob>
+    {/key}
+  </div>
+  <p>Path: {path}</p>
+  <div class="block">
+    <label for="pointAmount">Amount of points: </label>
+    <input id="pointAmount" type="range" min=3 step=1 max=40 bind:value={pointAmount}>
+    {pointAmount}
+  </div>
+  <div class="block">
+    <label for="deformation">Deformation: </label>
+    <input id="deformation" type="range" min=0 max=1 step=0.01 bind:value={deformation}>
+    {deformation}
+  </div>
+  <div class="block">
+    <label for="debugCircles">Debug circles: </label>
+    <input id="debugCircles" type="checkbox" bind:checked={debugCircles}>
+    {debugCircles}
+  </div>
+  <div class="block">
+    <label for="debugCircleRadius">Debug circle radius: </label>
+    <input id="debugCircleRadius" disabled={!debugCircles} type="range" min=1 max=10 bind:value={debugCircleRadius}>
+    {debugCircleRadius}
+  </div>
+  <div class="block">
+    <button on:click={() => key = {}}>Randomize</button>
+  </div>
+</div>
